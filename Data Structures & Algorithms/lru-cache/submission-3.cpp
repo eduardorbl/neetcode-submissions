0fcc8949f0ea7dfed519cf1d2b7647f7c7dbc4bf
José Eduardo Santos Rabelo <127237956+eduardorbl@users.noreply.github.com>
2026-05-31 23:21:32 -0300

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.cpp
@@ -0,0 +1,42 @@
+class LRUCache {
+    unordered_map<int, int> memory; // memory -> value
+    unordered_map<int, list<int>::iterator> keyToIt;
+    list<int> listMemory;
+    int cap;
+public:
+    LRUCache(int capacity) {
+        memory = unordered_map<int, int>();
+        listMemory = list<int>();
+        keyToIt = unordered_map<int, list<int>::iterator>();
+        cap = capacity;
+    }
+    
+    int get(int key) { // O(1)
+        if (memory.contains(key))
+        {
+            list<int>::iterator it = keyToIt[key];
+            listMemory.erase(it);
+            listMemory.push_front(key);
+            keyToIt[key] = listMemory.begin();
+            return memory[key];
+        } else
+        {
+            return -1;
+        }
+    }
+    
+    void put(int key, int value) { // O(1)
+        cout << listMemory.size() << "\n";
+        if (listMemory.size() == cap)
+        {
+            int keyDeletedItem = listMemory.back();
+            listMemory.pop_back();
+            memory.erase(keyDeletedItem);
+            keyToIt.erase(keyDeletedItem);
+        }
+        listMemory.push_front(key);
+        keyToIt[key] = listMemory.begin();
+        memory[key] = value;
+        cout << listMemory.size() << "\n";
+    }
+};
