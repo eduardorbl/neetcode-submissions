d32c837f9936cc1d9d929f637deb5a3c9532602a
José Eduardo Santos Rabelo <127237956+eduardorbl@users.noreply.github.com>
2026-06-03 12:51:28 -0300

    Add: meeting-schedule-ii - submission-17

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-17.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+
+    bool compareIntervals (const Interval& a, const Interval& b)
+    {
+        return (a.start <= b.start);
+    }
+    int minMeetingRooms(vector<Interval>& intervals) {
+        // im considering that intervals is sorted by begin (gotta sort to make sure)
+        // heap that contains the min finish time on the open rooms
+        // loop 
+        //      -> check if heap top is smaller than my start time -> started after the end
+        //      -> in casse is false: open 1 room and put into the heap -> get the maximum size of all heaps
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b){return (a.start < b.start);});
+        int ans = 0;
+        for (Interval interval : intervals)
+        {
+            if (!minHeap.empty() && minHeap.top() <= interval.start) 
+            {
+                minHeap.pop();
+                minHeap.push(interval.end);
+            } else
+            {
+                minHeap.push(interval.end);
+                ans = max(ans, (int)minHeap.size());
+            }
+        }
+        return ans;
+    }
+};
