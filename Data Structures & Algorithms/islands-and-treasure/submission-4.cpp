bd122f93eb2c64ea8ee11ff0ea747d742e2f1eb8
José Eduardo Santos Rabelo <127237956+eduardorbl@users.noreply.github.com>
2026-06-03 23:03:57 -0300

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        long long inf = 2147483647; 
+        vector<pair<int, int>> dirs = {
+            {-1,0}, {1,0}, {0,-1}, {0,1}       
+        };
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        queue<pair<int, int>> q;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j]==0) q.push({i, j});
+            }
+        }
+        int dist = 1;
+        while (!q.empty())
+        {
+            int size = q.size();
+            while(size--)
+            {
+                auto [i, j] = q.front();
+                q.pop();
+
+                for (auto [dx, dy] : dirs)
+                {
+                    int ni = dx + i, nj = dy + j;
+                    if (ni < 0 || ni >= n || nj < 0 || nj >= m) continue;
+                    if (grid[ni][nj] != inf) continue;
+                    grid[ni][nj] = dist;
+                    q.push({ni,nj});
+                }
+            }
+            dist++;
+        }
+    }
+};
