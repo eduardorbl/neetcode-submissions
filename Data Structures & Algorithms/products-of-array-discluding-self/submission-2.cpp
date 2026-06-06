114998b98472a41da4658a9a1014d437ab70d19b
José Eduardo Santos Rabelo <127237956+eduardorbl@users.noreply.github.com>
2026-06-06 15:43:36 -0300

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int prodLeft = 1;
+        int prodRight = 1;
+        vector<int> ans(nums.size(), 1);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans[i]=prodLeft;
+            ans[nums.size()-1-i]=prodRight;
+            prodLeft *= nums[i];
+            prodRight *= nums[nums.size()-1-i];
+        }
+        return nums;
+    }
+};
