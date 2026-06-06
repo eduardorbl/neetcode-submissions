b1aca405e967e7c4978c857bfd4c0222736da512
José Eduardo Santos Rabelo <127237956+eduardorbl@users.noreply.github.com>
2026-06-05 23:31:50 -0300

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> setNums;
+        for (int i : nums) {if (setNums.contains(i)) {return false;} else setNums.push(i);}
+        return true;
+    }
+};
