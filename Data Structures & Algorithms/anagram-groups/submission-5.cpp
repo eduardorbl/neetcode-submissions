b681320702957e71c30f8f513b5170d8cf69ba3c
José Eduardo Santos Rabelo <127237956+eduardorbl@users.noreply.github.com>
2026-06-06 01:34:11 -0300

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> listOfAnagrams;
+        for (string s : strs) 
+        {
+            vector<int> freq(26,0);
+            for (char c : s) freq[c-'a']++;
+            string sortedS = "";
+            for (int i = 0; i<26; i++) 
+            {
+                if (freq[i]>0) sortedS += static_cast<char>(i+'a');
+            }
+            cout << sortedS << '\n';
+            listOfAnagrams[sortedS].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for (const auto& [_, strings]: listOfAnagrams)
+        {
+            vector<string> anagrams;
+            for (string s : strings) anagrams.push_back(s);
+            ans.push_back(anagrams);
+        }
+        return ans;
+    }
+};
