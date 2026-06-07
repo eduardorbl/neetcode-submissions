e91fd5ed2c0480ba1616c340e8cb9e4c5a0c4b31
José Eduardo Santos Rabelo <127237956+eduardorbl@users.noreply.github.com>
2026-06-07 01:53:54 -0300

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for (char c : s)
+        {
+            char lower = static_cast<char>(tolower(c));
+            if (lower <= 'z' && lower >= 'a') str += c;
+        }
+        cout << str;
+        int lim = str.size() - ((str.size()%2) ? 1 : 0);
+        for (int i = 0; i < lim; i++)
+        {
+            if (str[i] != str[str.size()-i-1]) return false;
+        }
+        return true;
+    }
+};
