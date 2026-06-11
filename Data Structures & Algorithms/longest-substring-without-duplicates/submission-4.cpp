0df3bf75ee0b54384ceea228e416349dfa0337fc
José Eduardo Santos Rabelo <127237956+eduardorbl@users.noreply.github.com>
2026-06-11 17:07:56 -0300

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        if (n == 1) return 1;
+        int maxSize = 1;
+        vector<int> freq(128, 0);
+        int l = 0;
+        freq[0]++;
+        // freqz = 0, freqx = 1, freqy = 1, maxsize = 3
+        for (int r = 1; r < n; r++)
+        {
+            while (freq[s[r]] > 0)
+            {
+                freq[s[l]]--;
+                l++;
+            }
+            freq[s[r]]++;
+            maxSize = max(maxSize, r-l+1);
+            cout << maxSize << "\n";
+        }
+        return maxSize;
+    }
+};
